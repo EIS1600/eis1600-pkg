fc26346e2d912002597f5c26e89f2184a1fc0494
Lisa Mischer <17195420+lmischer@users.noreply.github.com>
2022-12-02 09:57:06 +0100

    export dates module

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='eis1600',
-      version='0.5.3',
+      version='0.5.4',
       description='EIS1600 project tools and utilities',
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -13,20 +13,21 @@ setup(name='eis1600',
       author_email='mischer.lisa@gmail.com',
       license='MIT License',
       packages=['eis1600',
+                'eis1600.dates',
                 'eis1600.helper',
-                'eis1600.miu',
                 'eis1600.markdown',
+                'eis1600.miu',
                 'eis1600.nlp'],
       entry_points={
           'console_scripts': [
                   'convert_mARkdown_to_EIS1600TMP = eis1600.markdown.convert_mARkdown_to_EIS1600TMP:main',
-                  'insert_uids = eis1600.markdown.insert_uids:main',
-                  'update_uids = eis1600.markdown.update_uids:main',
-                  'xx_update_uids_old_process = eis1600.markdown.update_uids_old_process:main',
                   'disassemble_into_miu_files = eis1600.miu.disassemble_into_miu_files:main',
-                  'reassemble_from_miu_files = eis1600.miu.reassemble_from_miu_files:main',
+                  'insert_uids = eis1600.markdown.insert_uids:main',
+                  'miu_random_revisions = eis1600.helper.miu_random_revisions:main',
                   'ner_annotate_mius = eis1600.nlp.ner_annotate_mius:main',
-                  'miu_random_revisions = eis1600.helper.miu_random_revisions:main'
+                  'reassemble_from_miu_files = eis1600.miu.reassemble_from_miu_files:main',
+                  'update_uids = eis1600.markdown.update_uids:main',
+                  'xx_update_uids_old_process = eis1600.markdown.update_uids_old_process:main'
           ],
       },
       python_requires='>=3.7, <3.9',

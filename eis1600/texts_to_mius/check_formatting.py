ace8629e01be7b16b98a1af1955d4a1a41348401
Lisa Mischer <17195420+lmischer@users.noreply.github.com>
2024-01-19 10:12:39 +0100

    Update ids_insert_or_update

--- eis1600/texts_to_mius/check_formatting.py
+++ eis1600/texts_to_mius/check_formatting.py
@@ -1,7 +1,14 @@
 from sys import argv
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from glob import glob
+from logging import Formatter, INFO
+
+from pandas import concat
+from tqdm import tqdm
 
 from eis1600.helper.CheckFileEndingActions import CheckFileEndingEIS1600OrEIS1600TMPAction
+from eis1600.helper.logging import setup_logger
+from eis1600.repositories.repo import TEXT_REPO, get_ready_and_double_checked_texts
 from eis1600.texts_to_mius.check_formatting_methods import check_formatting
 
 
@@ -20,4 +27,42 @@ def main():
 
     infile = args.input
 
-    check_formatting(infile)
+    if infile:
+        check_formatting(infile)
+    else:
+        df_ready, df_double_checked = get_ready_and_double_checked_texts()
+        df_files = concat([df_ready, df_double_checked])
+        files = []
+
+        missing_texts = []
+        for uri in df_files['Book Title']:
+            author, text = uri.split('.')
+            text_path = TEXT_REPO + 'data/' + author + '/' + uri + '/'
+            text_file = glob(text_path + '*.EIS1600')
+            if text_file:
+                files.append(text_file[0])
+            else:
+                missing_texts.append(uri)
+
+        if missing_texts:
+            print('URIs for files for whom no .EIS1600 file was found')
+            for uri in missing_texts:
+                print(uri)
+            print('\n')
+
+        formatter = Formatter('%(message)s\n\n\n')
+        logger = setup_logger('mal_formatted_texts', TEXT_REPO + 'mal_formatted_texts.log', INFO, formatter)
+        print('Check formatting for double-checked and ready texts')
+
+        count = 0
+        for text in tqdm(files):
+            try:
+                check_formatting(text)
+            except ValueError as e:
+                count += 1
+                logger.error(e)
+            except FileNotFoundError:
+                print(f'Missing: {text}')
+
+        logger.info(f'\n\n\n{count}/{len(files)} files need fixing')
+
--- eis1600/texts_to_mius/subid_methods.py
+++ eis1600/texts_to_mius/subid_methods.py
@@ -22,7 +22,6 @@ def pre_clean_text(text: str) -> str:
     text = text.replace(' \n', '\n')
     text = text.replace('\n ', '\n')
     text = PAGE_TAG_ON_NEWLINE_TMP_PATTERN.sub(r' \1', text)
-    text = MISSING_DIRECTIONALITY_TAG_PATTERN.sub('\g<1>_ء_ \g<2>', text)
 
     return text
 
@@ -38,7 +37,7 @@ def insert_ids(text: str) -> str:
     paragraph = next(text_iter)
     prev_p = ''
 
-    subids = None
+    sub_ids = None
 
     # Insert UIDs and EIS1600 tags into the text
     while paragraph is not None:
@@ -48,14 +47,14 @@ def insert_ids(text: str) -> str:
             # Only do this if paragraph is not empty
             if paragraph.startswith('#'):
                 uid = uids.get_uid()
-                subids = SubIDs(uid)
+                sub_ids = SubIDs(uid)
                 # Move content to an individual line
                 paragraph = BIO_CHR_TO_NEWLINE_PATTERN.sub(r'\1\n\2', paragraph)
                 paragraph = paragraph.replace('#', f'_ء_#={uid}=')
                 # Insert a paragraph tag
                 heading_and_text = paragraph.splitlines()
                 if len(heading_and_text) == 2:
-                    paragraph = heading_and_text[0] + f'\n\n_ء_={subids.get_id()}= ::UNDEFINED:: ~\n_ء_ ' + \
+                    paragraph = heading_and_text[0] + f'\n\n_ء_={sub_ids.get_id()}= ::UNDEFINED:: ~\n_ء_ ' + \
                                 heading_and_text[1]
                 elif len(heading_and_text) > 2:
                     raise ValueError(
@@ -63,7 +62,7 @@ def insert_ids(text: str) -> str:
                     )
                 text_updated.append(paragraph)
             elif '%~%' in paragraph:
-                paragraph = f'_ء_={subids.get_id()}= ::POETRY:: ~\n_ء_ ' + '\n_ء_ '.join(paragraph.splitlines())
+                paragraph = f'_ء_={sub_ids.get_id()}= ::POETRY:: ~\n_ء_ ' + '\n_ء_ '.join(paragraph.splitlines())
                 text_updated.append(paragraph)
             elif PAGE_TAG_PATTERN.fullmatch(paragraph):
                 page_tag = PAGE_TAG_PATTERN.match(paragraph).group('page_tag')
@@ -81,7 +80,7 @@ def insert_ids(text: str) -> str:
                 p_pieces = paragraph.splitlines()
                 section_header = p_pieces[0]
 
-                if '%' in paragraph:
+                if '%~%' in paragraph:
                     paragraph = '\n_ء_ '.join(p_pieces[1:])
                 elif len(p_pieces) > 2:
                     raise ValueError(
@@ -95,10 +94,10 @@ def insert_ids(text: str) -> str:
                             '::\\n[^هسءگؤقأذپيمجثاڤوضآرتنكزفبعٱشىصلدطغإـئظحةچخ_]'
                     )
 
-                paragraph = f'_ء_={subids.get_id()}= {section_header} ~\n_ء_ ' + paragraph
+                paragraph = f'_ء_={sub_ids.get_id()}= {section_header} ~\n_ء_ ' + paragraph
                 text_updated.append(paragraph)
             else:
-                paragraph = f'_ء_={subids.get_id()}= ::UNDEFINED:: ~\n_ء_ ' + paragraph
+                paragraph = f'_ء_={sub_ids.get_id()}= ::UNDEFINED:: ~\n_ء_ ' + paragraph
                 text_updated.append(paragraph)
 
         prev_p = paragraph
@@ -111,13 +110,13 @@ def insert_ids(text: str) -> str:
 
 
 def update_ids(text: str) -> str:
-    # disassemble text into paragraphs
+    # 1. disassemble text into paragraphs
     text = text.split('\n\n')
     text_updated = []
 
     used_ids = []
 
-    # Collect existing UIDs
+    # 2. Collect existing UIDs
     for idx, paragraph in enumerate(text):
         if MIU_UID_PATTERN.match(paragraph):
             uid = int(MIU_UID_PATTERN.match(paragraph).group('UID'))
@@ -138,7 +137,7 @@ def update_ids(text: str) -> str:
     paragraph = next(text_iter)
     prev_p = None
 
-    # 1. Update MIU_UIDs (sub_ids are adjusted in the next step)
+    # 3. Update MIU_UIDs (sub_ids are adjusted in the next step) and fix some structural problems
     while paragraph is not None:
         next_p = next(text_iter, None)
 
@@ -152,8 +151,7 @@ def update_ids(text: str) -> str:
                 # Insert a paragraph tag
                 heading_and_text = paragraph.splitlines()
                 if len(heading_and_text) == 2:
-                    paragraph = heading_and_text[0] + f'\n\n::UNDEFINED:: ~\n\n_ء_ ' \
-                                                      f'{heading_and_text[1]}'
+                    paragraph = heading_and_text[0] + f'\n\n::UNDEFINED:: ~\n\n_ء_ {heading_and_text[1]}'
                 elif len(heading_and_text) > 2:
                     raise ValueError(
                             f'There is a single new line in this paragraph:\n{paragraph}'
@@ -162,8 +160,8 @@ def update_ids(text: str) -> str:
                 if paragraph.startswith('::'):
                     p_pieces = paragraph.splitlines()
                     section_header = p_pieces[0] + ' ~'
-                    if '%' in paragraph:
-                        paragraph = '\n_ء_ '.join(p_pieces[1:])
+                    if '%~%' in paragraph:
+                        paragraph = '\n'.join(p_pieces[1:])
                     elif len(p_pieces) > 2:
                         raise ValueError(
                                 f'There is a single new line in this paragraph:\n{paragraph}'
@@ -172,50 +170,39 @@ def update_ids(text: str) -> str:
                         paragraph = p_pieces[1]
                     else:
                         raise ValueError(
-                                'There is an empty paragraph, check with\n'
-                                '::\\n[^هسءگؤقأذپيمجثاڤوضآرتنكزفبعٱشىصلدطغإـئظحةچخ_]'
+                                'There is an empty paragraph'
                         )
-
-                    if paragraph[0] != '_':
-                        paragraph = '_ء_ ' + paragraph
                 else:
-                    cat = 'POETRY' if '%' in paragraph else 'UNDEFINED'
-                    if paragraph[0] != '_':
-                        paragraph = '_ء_ ' + paragraph
+                    cat = 'POETRY' if '%~%' in paragraph else 'UNDEFINED'
                     section_header = f'::{cat}:: ~'
+
                 paragraph = f'{section_header}\n' + paragraph
             elif MIU_UID_TAG_AND_TEXT_SAME_LINE_PATTERN.match(paragraph):
-                # TODO adjust sub_ids of all paragraphs inside MIU
                 paragraph = MIU_UID_TAG_AND_TEXT_SAME_LINE_PATTERN.sub(r'\1\2\n\3', paragraph)
                 # Insert a paragraph tag
                 heading_and_text = paragraph.splitlines()
-                if '%' in '\n'.join(heading_and_text[1:]):
-                    paragraph = heading_and_text[0] + f'\n\n::POETRY:: ~\n\n_ء_' + '\n_ء_ ' \
-                                                                                                           ''.join(
-                            heading_and_text[1:]
-                    )
+                if '%~%' in '\n'.join(heading_and_text[1:]):
+                    paragraph = heading_and_text[0] + f'\n\n::POETRY:: ~\n_ء_ ' + '\n'.join(heading_and_text[1:])
                 elif len(heading_and_text) > 2:
                     raise ValueError(
                             f'There is a single new line in this paragraph:\n{paragraph}'
                     )
                 else:
-                    paragraph = heading_and_text[0] + f'\n\n::UNDEFINED:: ~\n_ء_ ' + \
-                                heading_and_text[1]
+                    paragraph = heading_and_text[0] + f'\n\n::UNDEFINED:: ~\n_ء_ ' + heading_and_text[1]
             elif MIU_TAG_AND_TEXT_PATTERN.match(paragraph):
                 # TODO
                 uid = MIU_TAG_AND_TEXT_PATTERN.match(paragraph).group('UID')
                 sub_ids = SubIDs(uid)
                 # Insert a paragraph tag
                 heading_and_text = paragraph.splitlines()
-                if '%' in '\n'.join(heading_and_text[1:]):
-                    paragraph = heading_and_text[0] + f'\n\n::POETRY:: ~\n_ء_ ' + '\n_ء_ '.join(heading_and_text[1:])
+                if '%~%' in '\n'.join(heading_and_text[1:]):
+                    paragraph = heading_and_text[0] + f'\n\n::POETRY:: ~\n_ء_ ' + '\n'.join(heading_and_text[1:])
                 elif len(heading_and_text) > 2:
                     raise ValueError(
                             f'There is a single new line in this paragraph:\n{paragraph}'
                     )
                 else:
-                    paragraph = heading_and_text[0] + f'\n\n::UNDEFINED:: ~\n_ء_ ' + \
-                                heading_and_text[1]
+                    paragraph = heading_and_text[0] + f'\n\n::UNDEFINED:: ~\n_ء_ ' + heading_and_text[1]
 
             if ONLY_PAGE_TAG_PATTERN.fullmatch(paragraph):
                 # Add page tags to previous paragraph if there is no other information contained in the current
@@ -232,10 +219,6 @@ def update_ids(text: str) -> str:
                         next_p = next(text_iter, None)
                 elif text_updated:
                     text_updated[-1] += ' ' + page_tag
-                # else:
-                # Remove PageV00P000 at the beginning in an individual paragraph
-                # text_updated.append(paragraph)
-                # pass
             elif not PARAGRAPH_UID_PATTERN.fullmatch(paragraph):
                 # Do not add empty paragraphs to the updated text
                 text_updated.append(paragraph)
@@ -243,7 +226,7 @@ def update_ids(text: str) -> str:
             prev_p = paragraph
             paragraph = next_p
 
-    # 2. Update sub_ids
+    # 4. Update sub_ids
     text = '\n\n'.join(text_updated)
     mius = MIU_SPLITTER_PATTERN.split(text)[1:]     # First element is None because the regex matches the text beginning
     uids = [elem for idx, elem in enumerate(mius) if idx % 2 == 0]
@@ -260,14 +243,26 @@ def update_ids(text: str) -> str:
             if paragraph.startswith('::'):
                 p_pieces = paragraph.splitlines()
                 section_header = p_pieces[0]
-                paragraph = p_pieces[1]
+                if '%~%' in paragraph:
+                    paragraph = '\n'.join(p_pieces[1:])
+                elif len(p_pieces) > 2:
+                    raise ValueError(
+                            f'There is a single new line in this paragraph:\n{paragraph}'
+                    )
+                elif len(p_pieces) == 2:
+                    paragraph = p_pieces[1]
+                else:
+                    raise ValueError(
+                            'There is an empty paragraph, check with\n'
+                            '::\\n[^هسءگؤقأذپيمجثاڤوضآرتنكزفبعٱشىصلدطغإـئظحةچخ_]'
+                    )
                 paragraph = f'_ء_={sub_ids.get_id()}= {section_header} ~\n' + paragraph
             elif not MIU_UID_PATTERN.match(paragraph):
                 paragraph = f'_ء_={sub_ids.get_id()}= ::UNDEFINED:: ~\n' + paragraph
 
             text_updated.append(paragraph)
 
-    # reassemble text
+    # 5. Reassemble text
     text = '\n\n'.join(text_updated)
     return text
 
@@ -279,29 +274,45 @@ def add_ids(infile: str, ids_update: Optional[bool] = False) -> None:
     :param str infile: Path of the file to convert.
     :return None:
     """
+    file_path, file_ext = splitext(infile)
 
-    with open(infile, 'r+', encoding='utf8') as infile_h:
-        text = infile_h.read()
+    if file_ext == '.EIS1600':
+        # EIS1600 file extension
+        with open(infile, 'r+', encoding='utf8') as infile_h:
+            text = infile_h.read()
+
+            header_and_text = HEADER_END_PATTERN.split(text)
+            header = header_and_text[0] + header_and_text[1]
+            text = header_and_text[2].lstrip('\n')  # Ignore new lines after #META#Header#End#
+            text = pre_clean_text(text)
+            text = MISSING_DIRECTIONALITY_TAG_PATTERN.sub('\g<1>_ء_ \g<2>', text)
+
+            check_file_for_mal_formatting(infile, text)
+            text = update_ids(text)
+
+            final = header + '\n\n' + text
+            if final[-1] != '\n':
+                final += '\n'
+
+            infile_h.seek(0)
+            infile_h.write(final)
+            infile_h.truncate()
+    else:
+        # EIS1600TMP file extension
+        with open(infile, 'r', encoding='utf8') as infile_h:
+            text = infile_h.read()
 
         header_and_text = HEADER_END_PATTERN.split(text)
         header = header_and_text[0] + header_and_text[1]
         text = header_and_text[2].lstrip('\n')  # Ignore new lines after #META#Header#End#
         text = pre_clean_text(text)
 
-        file_path, file_ext = splitext(infile)
-
-        if file_ext == 'EIS1600':
-            # EIS1600TMP file extension
-            check_file_for_mal_formatting(infile, text)
-            text = update_ids(text)
-        else:
-            # EIS1600TMP file extension
-            text = insert_ids(text)
+        text = insert_ids(text)
 
         final = header + '\n\n' + text
         if final[-1] != '\n':
             final += '\n'
 
-        infile_h.seek(0)
-        infile_h.write(final)
-        infile_h.truncate()
+        outfile = infile.replace('TMP', '')
+        with open(outfile, 'w', encoding='utf8') as outfile_h:
+            outfile_h.write(final)
